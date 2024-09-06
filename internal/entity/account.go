80b3e7070b9df6d1c70cbf81c05a7ad965fc7299
josehpena <jose.henrique.pena.goulart@gmail.com>
2024-09-06 18:06:17 -0300

    Creating account(feat): Feat to create a account with a not empty client

---
+++ internal/entity/account.go
@@ -0,0 +1,29 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Account struct {
+	id        string
+	Client    *Client
+	Balance   float64
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func NewAccount(client *Client) *Account {
+	if client == nil {
+		return nil
+	}
+	account := &Account{
+		id:        uuid.New().String(),
+		Client:    client,
+		Balance:   0,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	return account
+}
